85687f57c8a83367ee685ba2fdb1d60e0afba5d5
Andrey Souza <134808243+andrey-alm@users.noreply.github.com>
2023-06-18 22:29:36 -0300

    Alterações gerais

--- js/index.js
+++ js/index.js
@@ -1,29 +1,3 @@
-let ham = document.querySelector(".cabecalho-ham")
-let menu = document.querySelector(".cabecalho-menu")
-let abrir = document.querySelector(".abrir")
-let fechar = document.querySelector(".fechar")
-ham.addEventListener("click", abrirMenu)
-
-function abrirMenu() {
-    if (menu.style.display == "block") {
-        menu.style.display = "none"
-        abrir.style.display = "block"
-        fechar.style.display = "none"
-    } else {
-        menu.style.display = "block"
-        fechar.style.display = "block"
-        abrir.style.display = "none"
-    }
-}
-
-function adequarMenu() {
-    if (window.innerWidth >= 768) {
-        menu.style.display = "block"
-    } else {
-        menu.style.display = "none"
-    }
-}
-
 // código de teste
 
 let btnEnviar = document.querySelector("#form-submit")

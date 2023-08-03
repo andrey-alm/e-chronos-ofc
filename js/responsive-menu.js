c7564aa1a65bb22e0c6c9b74b73453c5d982cdc0
Andrey Souza <134808243+andrey-alm@users.noreply.github.com>
2023-08-02 22:20:28 -0300

    Novo menu e pequenas alterações

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -1,25 +1,56 @@
+let cabecalho = document.querySelector(".cabecalho")
+let img = document.querySelector(".cabecalho-imagem")
 let ham = document.querySelector(".cabecalho-ham")
 let menu = document.querySelector(".cabecalho-menu")
 let abrir = document.querySelector(".abrir")
 let fechar = document.querySelector(".fechar")
-ham.addEventListener("click", abrirMenu)
+
+ham.addEventListener("click", testeMenu)
+
+function testeMenu() {
+    if (menu.style.flexDirection == "column") {
+        abrirMenu()
+    } else {
+        menu.style.display = "flex"
+        menu.style.flexDirection = "row"
+        abrirMenu()
+    }
+}
 
 function abrirMenu() {
-    if (menu.style.display == "block") {
+    if (menu.style.flexDirection == "column") {
+        cabecalho.style.flexDirection = "row"
+        cabecalho.style.padding = "0.75rem"
+        menu.style.gap = "1.875rem"
+        menu.style.flexDirection = "row"
         menu.style.display = "none"
-        abrir.style.display = "block"
+        img.style.display = "block"
+        abrir.style.display = "flex"
         fechar.style.display = "none"
     } else {
-        menu.style.display = "block"
-        fechar.style.display = "block"
+        cabecalho.style.flexDirection = "column"
+        cabecalho.style.padding = "0"
+        menu.style.flexDirection = "column"
+        menu.style.gap = "0"
+        img.style.display = "none"
+        fechar.style.display = "flex"
         abrir.style.display = "none"
     }
 }
 
 function adequarMenu() {
     if (window.innerWidth >= 768) {
-        menu.style.display = "block"
+        menu.style.display = "flex"
+        menu.style.flexDirection = "row"
+        menu.style.gap = "1.875rem"
+        cabecalho.style.padding = "1.25rem"
+        cabecalho.style.flexDirection = "row"
     } else {
+        abrir.style.display = "flex"
         menu.style.display = "none"
+        cabecalho.style.padding = "0.75rem"
+        img.style.display = "flex"
+        img.style.flexDirection = "row"
+        fechar.style.display = "none"
     }
 }
